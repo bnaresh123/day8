50d2da8ad2c9341640c94904f79c0b6939956343
Govardhan <Govardhan>
2016-09-14 10:57:44 +0530

    govardhan first commit created pack2

---
+++ src/Pack2/RevStringFunction.java
@@ -0,0 +1,9 @@
+package Pack2;
+
+public class RevStringFunction {
+	public static void main(String[] args) {
+		System.out.println("this is my rev string function class");
+		System.out.println("In this function I am adding some code here");
+	}
+
+}
